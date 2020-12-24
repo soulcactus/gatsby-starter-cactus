053b56107fa5ac55e2e9be52d3af903cf40939c8
soulcactus <sunhyejin90@naver.com>
2020-12-24 18:49:54 +0900

    🔧 Fix : Fix Style bugs

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -67,6 +67,7 @@ const StyledSwitch = styled.button`
         left: var(--size-0);
         ${$size('var(--size-30)')};
         border-radius: var(--size-percent-50);
+        background: var(--color-main);
         font-size: var(--size-20);
         color: var(--color-sub-text-1);
         box-shadow: var(--box-shadow-color-6);
